64d7b856820d1e1b9f2a924bd25fda655161f903
seg7577 <seg082911@gmail.com>
2025-07-12 03:46:51 +0900

    demo1

--- src/component/headerBar/headerBar.js
+++ src/component/headerBar/headerBar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { HiMenu, HiX } from 'react-icons/hi';
 import './headerBar.css';
 import logoImg from '../../img/logo192.png';
 
 function HeaderBar({isMobile}) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header className="header-bar">
@@ -11,6 +14,7 @@ function HeaderBar({isMobile}) {
           <Link to="/"><img src={logoImg} className="logo-img" alt="로고" /></Link>
           <Link to="/" className="logo-title">전북대학교 공과대학 학생회</Link>
         </div>
+        {!isMobile && (
         <nav className="nav-menu">
           <ul className="nav-list">
             <li><Link to="/about">학생회소개</Link></li>
@@ -20,6 +24,32 @@ function HeaderBar({isMobile}) {
             <li><Link to="/resources">자료실</Link></li>
           </ul>
         </nav>
+        )}
+
+        {isMobile && (
+        <>
+          <button className="menu-button" onClick={() => setMenuOpen(true)} aria-label="메뉴 열기">
+            <HiMenu />
+          </button>
+          {/* 사이드 메뉴 오픈 시 표시 */}
+          {menuOpen && (
+            <div className="mobile-menu-overlay">
+              <div className="mobile-menu">
+                <button className="close-button" onClick={() => setMenuOpen(false)} aria-label="메뉴 닫기">
+                  <HiX />
+                </button>
+                <ul className="mobile-nav-list">
+                  <li><Link to="/about" onClick={() => setMenuOpen(false)}>학생회소개</Link></li>
+                  <li><Link to="/notice" onClick={() => setMenuOpen(false)}>알림</Link></li>
+                  <li><Link to="/benefits" onClick={() => setMenuOpen(false)}>제휴 혜택</Link></li>
+                  <li><Link to="/contact" onClick={() => setMenuOpen(false)}>문의</Link></li>
+                  <li><Link to="/resources" onClick={() => setMenuOpen(false)}>자료실</Link></li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </>
+      )}
       </header>
     </>
   );
