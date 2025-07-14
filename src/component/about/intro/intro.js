c658174c8374881cde090a6e6349b9381033f204
seg7577 <seg082911@gmail.com>
2025-07-15 03:16:54 +0900

    구조 분할

---
+++ src/component/about/intro/intro.js
@@ -0,0 +1,26 @@
+
+function Intro (){
+    return(
+    <div className="intro-container">
+        <div className="imageSlider-container">
+
+        </div>
+        <div className="introduce-container">
+
+        </div>
+        <div className="main-container">
+            <div className="greetings-container">
+                <h1>회장단 인사말</h1>
+            </div>
+            <div className="image-container">
+
+            </div>
+            <div className="text-container">
+
+            </div>
+        </div>
+    </div>
+);
+}
+
+export default Intro;
--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HeaderBar from '../headerBar/headerBar.js';
+import './home.css';
 
 import image1 from '../../img/imageSlider1.png';
 import image2 from '../../img/imageSlider2.png';
 import image3 from '../../img/imageSlider3.png';
 import ImageSlider from './imageSlider/imageSlider.js';
-
-import './home.css';
 
 import CardSection from './cardSection/cardSection.js';
 
@@ -25,56 +24,58 @@ import Banner from '../../img/banner.png';
 
 import CustomCalendar from './customCalendar/customCalendar.js';
 import { HiMenu } from 'react-icons/hi';
+
+import Intro from '../about/intro/intro.js';
 function Home() {
   const slides = [{image: image1}, {image: image2}, {image: image3}];
   const { isMobile, isTablet, isDesktop } = useResponsive();
   useLayoutResize();
     return(
-      <Router>
-        <div className="app-container">
-          <div id= "header-container" className='header-container'> 
-          <HeaderBar isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop} />            
-          <Routes>
-              <Route path="/about" element={ <About/> } />
-              <Route path="/notice" element={ <Notice/>} />
-              <Route path="/benefits" element={<Benefits/>} />
-              <Route path="/contact" element={<Contact/>} />
-              <Route path="/resources" element={<Resources/>} />
-              <Route path="/" element={<div></div>} />
-            </Routes>
-          </div>
+        <BrowserRouter>
+          <div className="app-container">
+            <div id= "header-container" className='header-container'> 
+            <HeaderBar isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop} /> 
+              <Routes>
+                  <Route path="/about" element={ <About/> } />
+                  <Route path="/notice" element={ <Notice/>} />
+                  <Route path="/benefits" element={<Benefits/>} />
+                  <Route path="/contact" element={<Contact/>} />
+                  <Route path="/resources" element={<Resources/>} />
+                  <Route path="/" element={<div/>} />
+                  <Route path="/about/intro" element={<Intro/>} />
+                </Routes>
+            </div>
 
-          <div className='first-screen'>
-            {/* 여기까지가 pc로 한 화면으로 나오게 부탁드립니다.. */}
-            <div id = "slider-container" className='slider-container'> <ImageSlider slides={slides} /> </div>
-            <div id = "cards-container" className='cards-container'>  <CardSection isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop}/> </div>
-          </div>
+            <div className='first-screen'>
+              {/* 여기까지가 pc로 한 화면으로 나오게 부탁드립니다.. */}
+              <div id = "slider-container" className='slider-container'> <ImageSlider slides={slides} /> </div>
+              <div id = "cards-container" className='cards-container'>  <CardSection isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop}/> </div>
+            </div>
 
-          <div className='second-screen'>
-            {/* 툴바 + 제휴업체 검색이 딱 한 화면에 나오도록 부탁드립니다. */}
-            <div className='map-container'>
-              <Kakao />
-              <div className='list-button'>
-                <button> <HiMenu/> 목록 보기</button>
+            <div className='second-screen'>
+              {/* 툴바 + 제휴업체 검색이 딱 한 화면에 나오도록 부탁드립니다. */}
+              <div className='map-container'>
+                <Kakao />
+                <div className='list-button'>
+                  <button> <HiMenu/> 목록 보기</button>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className='third-screen'>
-            {/* 툴바 + 행사 달력이 딱 한 화면에 나오도록 부탁드립니다. */}
-            <div className='calender-container'>
-              <CustomCalendar
-              />
+            <div className='third-screen'>
+              {/* 툴바 + 행사 달력이 딱 한 화면에 나오도록 부탁드립니다. */}
+              <div className='calender-container'>
+                <CustomCalendar
+                />
+              </div>
+            </div>
+
+
+            <div className='banner'>
+                <img src={Banner} className='banner-img' alt="배너"/>
             </div>
           </div>
-
-
-          <div className='banner'>
-              <img src={Banner} className='banner-img' alt="배너"/>
-          </div>
-        </div>
-      </Router>
-
+        </BrowserRouter>
     );
 }
 
